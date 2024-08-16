5ac95e75b1e3f11e5068991bdd2a2632ebd760ea
tanvir-hossen49 <thossen49@gmail.com>
2024-08-16 18:34:33 +0600

    added add button for add todo

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -6,8 +6,11 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer"
+import { PlusIcon } from "lucide-react";
 
-const Calender = ({
+const Calendar = ({
   weeksName,
   getWeekDays,
   currentDate,
@@ -15,44 +18,51 @@ const Calender = ({
 }) => {
   return (
     <div className="mt-5">
-      <Table className="">
-        <TableHeader className="dark:text-[#ffffff70]">
+      <Table className="w-full">
+        <TableHeader className="sticky top-0 dark:bg-[#1a1a1a] dark:text-[#ffffff70] z-10">
           <TableRow>
-            {weeksName.map(name => (
+            {weeksName.map((name) => (
               <TableHead key={name} className="text-center">
                 {name}
-              </TableHead>
+              </TableHead>  
             ))}
           </TableRow>
         </TableHeader>
 
         <TableBody>
-          <TableRow className="h-full">
+          <TableRow className="h-[calc(100vh-70px)]">
             {getWeekDays().map((date) => (
               <TableCell
                 key={date.day}
-                className="h-[100vh] w-full text-right align-top p-2"
-                style={{ height: '100vh', width: `${100 / getWeekDays().length}%` }} // Distribute equal space
+                className="h-full text-right align-top p-2 group" 
+                style={{ width: `${100 / 7}%` }}
               >
-                {date.day === new Date().getDate() && currentDate.getMonth() === currentMonth ? (
-                  <div className="flex justify-center items-center w-6 h-6 bg-white dark:bg-[#b15a27] rounded-full ml-auto">
-                    <span>{date.day}</span>
+                <div className="flex justify-between items-center">
+                  <div className="invisible group-hover:visible"> 
+                    <Button className="dark:bg-[#202020]" variant="outline" size="icon">
+                      <PlusIcon className="h-4 w-4"/>
+                    </Button>
                   </div>
-                ) : (
-                  <>
-                    {date.day}
-                    <div>{date.day === 1 ? date.monthName : ""}</div>
-                  </>
-                )}
+                  <div className="ml-auto text-base">
+                    {date.day === new Date().getDate() && currentDate.getMonth() === currentMonth ? (
+                      <div className="flex justify-center items-center w-7 h-7 bg-white dark:bg-[#b15a27] rounded-full">
+                        <span>{date.day}</span>
+                      </div>
+                    ) : (
+                      <>
+                        {date.day}
+                        <div>{date.day === 1 && date.monthName}</div>
+                      </>
+                    )}
+                  </div>
+                </div>
               </TableCell>
             ))}
           </TableRow>
         </TableBody>
-
-
       </Table>
     </div>
   );
 };
 
-export default Calender;
+export default Calendar;
